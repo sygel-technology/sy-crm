0a34efb11fd81f17f8780bed6568fca4ad5e37e5
sygel <alberto.martinez@sygel.es>
2024-04-29 12:03:51 +0200

    [T-5971][ADD]crm_sale_automatic_quotation_asynchronous_wizard module

---
+++ crm_sale_automatic_quotation_asynchronous_wizard/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Alberto Martínez <alberto.martinez@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "CRM Sale Automatic Quotation Asynchronous Wizard",
+    "summary": "Speed up the CRM Sale Automatic Quotation Wizard",
+    "version": "15.0.1.0.0",
+    "category": "CRM",
+    "website": "https://www.sygel.es",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'crm_sale_automatic_quotation',
+        'queue_job',
+    ],
+    "data": [
+        "wizards/crm_sale_automatic_quotation_wizard.xml",
+    ],
+}
