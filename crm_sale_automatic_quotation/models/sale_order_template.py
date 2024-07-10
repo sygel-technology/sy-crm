25bdc3ddb591f463686e213f86e27039963b807c
Manuel Florido <manuel.florido@sygel.es>
2024-07-10 16:25:15 +0200

    [T-5944][IMP] crm_autoassign,crm_group_sale_salesman_extension,crm_sale_automatic_quotation,crm_sale_automatic_quotation_asynchronous_wizard:pre-commit auto fixes

--- crm_sale_automatic_quotation/models/sale_order_template.py
+++ crm_sale_automatic_quotation/models/sale_order_template.py
@@ -1,18 +1,18 @@
 # Copyright 2023 Ángel García de la Chica Herrera <angel.garcia@sygel.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class SaleOrderTemplate(models.Model):
     _inherit = "sale.order.template"
 
     crm_automatic_quotation = fields.Boolean(
-        string='CRM Automatic Quotation',
+        string="CRM Automatic Quotation",
     )
     crm_automatic_domain = fields.Char(
-        string='Domain',
+        string="Domain",
     )
     cr_automatic_exclude_from_wizard = fields.Boolean(
-        string='Exclude from CRM Automatic Quotation Wizard',
+        string="Exclude from CRM Automatic Quotation Wizard",
     )
